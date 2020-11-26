54eea46349b2a49a38ba20d054d2ec01e3de51c1
Cuan O'Connor <cuanconner@gmail.com>
2020-11-26 20:44:44 +0000

    Added very basic player, movement and a testing plane

---
+++ 3DEndlessRunner/Assets/__Scripts/Player_Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horizontalMultiplier = 2;
+
+    private void FixedUpdate()
+    {
+        // Player moves forward at a fixed interval
+        Vector3 forwardMove = transform.forward * speed * Time.deltaTime;
+        // Horizontal movement based on player input
+        // Player will move horizontally at the defined factor MORE than the forward speed
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
