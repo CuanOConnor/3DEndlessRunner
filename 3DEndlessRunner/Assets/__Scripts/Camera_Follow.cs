56ed39392284275edc05790c8ca4b45f41f6bfaa
Cuan O'Connor <cuanconner@gmail.com>
2020-11-28 20:58:52 +0000

    added tile basic tile spawning and behaviour

---
+++ 3DEndlessRunner/Assets/__Scripts/Camera_Follow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player;
+    Vector3 offset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // maintains camera distance from the player
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The camera will follow the player, but also stay centered on the track
+        Vector3 targetPos = player.position + offset;
+        targetPos.x = 0;
+        transform.position = targetPos;
+    }
+}
---
+++ 3DEndlessRunner/Assets/__Scripts/GroundTile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>(); // accessing our GroundSpawner script
+    }
+
+    // will trigger on an collision
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2); // destroy the object after the player leaves (2 seconds)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
